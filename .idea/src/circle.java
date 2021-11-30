a471e0e8ddd12b8e8478aa35bc75a95d77bbc04a
Tang9696 <tangchao9696@gmail.com>
2021-11-30 21:08:15 +0300

    Circle

--- .idea/src/circle.java
+++ .idea/src/circle.java
@@ -5,6 +5,8 @@ public class circle {
          public void main(){
              double d = 2.0;
              System.out.println(d);
+             double k =1.0;
+             System.out.println(k);
              
          }
 }
